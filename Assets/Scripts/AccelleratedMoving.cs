e7b5ba93023862df9c1afbde19f9331867504f84
Andriy Oblivantsev <eslider@gmail.com>
2015-06-25 09:36:44 +0200

    add Accellerated Moving control

---
+++ Assets/Scripts/AccelleratedMoving.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AccelleratedMoving : MonoBehaviour {
+
+	public int speedFactor = 20;
+	// Update is called once per frame
+	void Update () 
+	{
+
+		var gtext = GameObject.Find ("statusText");
+		var text = gtext.GetComponent<Text> ();
+		text.text = Input.acceleration.x + "x" + Input.acceleration.y + "x" + Input.acceleration.z;
+
+		transform.Translate(Input.acceleration.x/speedFactor, 0, -(Input.acceleration.z/speedFactor));
+
+
+	}
+}
