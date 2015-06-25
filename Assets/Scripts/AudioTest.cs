9d9c16e644a55ad6615150237682599039d53335
Andriy Oblivantsev <eslider@gmail.com>
2015-06-25 09:37:06 +0200

    add AudioTest control

---
+++ Assets/Scripts/AudioTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+*
+ */
+public class AudioTest : MonoBehaviour {
+    public int halloWorld = 2;
+
+    // Use this for initialization
+    void Start() {
+        var gameObject = GameObject.Find("enzo");
+        var audio = gameObject.GetComponent(typeof(AudioSource));
+        (audio as AudioSource).Play();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
