944b7f8768d2530ca1f35ef6f17af4669b428b72
Andriy Oblivantsev <eslider@gmail.com>
2015-06-25 09:38:18 +0200

    add advanced CameraTexture

---
+++ Assets/Scripts/CameraTexture.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CameraTexture : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		WebCamTexture webcamTexture = new WebCamTexture();
+		Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webcamTexture;
+		webcamTexture.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
